11e64bc6a492a2afe91f16bf1edb77666fd88c5b
maria <mgarciasierragrsr@gmail.com>
2025-03-07 09:06:30 +0100

    created spring project

---
+++ proyecto/src/test/java/cap/maria/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.maria.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
