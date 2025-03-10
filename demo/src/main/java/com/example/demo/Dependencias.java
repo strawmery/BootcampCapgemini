91cb4e5bb9fdb366473456b33be8d1321e89270a
maria <mgarciasierragrsr@gmail.com>
2025-03-10 08:32:46 +0100

    Add Dependencias configuration class with repository beans

---
+++ demo/src/main/java/com/example/demo/Dependencias.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Dependencias {
+	@Bean
+	Repositorio repositorio(Configuracion config, Registro registro) {
+		System.err.println("soy el bean");
+		//return new RepositorioMock();
+		return new RepositorioImpl(config, registro);
+	}
+	@Bean
+	Repositorio repo1(Configuracion config, Registro registro) {
+		return new RepositorioImpl(config, registro);
+	}
+	@Bean
+	Repositorio repo2() {
+		return new RepositorioMock();
+	}
+}
