56df2e44e29ecc0813c19437235adab51fd6e5bd
maria <mgarciasierragrsr@gmail.com>
2025-03-11 16:28:11 +0100

    Refactor GildedRose updateQuality method for improved readability and maintainability

--- GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,61 +8,83 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-
-                if (items[i].quality > 0) {
-
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-
-                }
-
-            } //condicion de bajar la calidad si no tiene ninguna condicion especial
-            
-            else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        } //condicion de backstage passes
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }//condicion de backstage passes
-                    }
-                }
-
+        for (Item item : items){
+            if(item.name.equals("Sulfuras, Hand of Ragnaros")){
+                continue;
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }//lo que no sea sulfuras baja
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            decreaseSellIn(item);
+            switch(item.name){
+                case "Aged Brie":
+                    qualityAgedBrie(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    qualityBackstagePasses(item);
+                    break;
+                case "Conjured":
+                    qualityConjured(item);
+                    break;
+                default:
+                    qualityNormalItem(item);
+                    break;
             }
+        }
+    }
+
+    private void increaseQuality(Item item){
+        if(item.quality < 50){
+            item.quality++;
+        }else{
+            item.quality = 50;
+            throw new IllegalArgumentException("Quality cannot be more than 50");
+        }
+    }
+
+    private void decreaseQuality(Item item){
+        if(item.quality > 0){
+            item.quality--;
+        }else{
+            item.quality = 0;
+            throw new IllegalArgumentException("Quality cannot be negative");
+        }
+    }
+
+    private void decreaseSellIn(Item item){
+        item.sellIn--;
+    }
+
+    private void qualityAgedBrie(Item item){
+        increaseQuality(item);
+        if(item.sellIn < 0){
+            increaseQuality(item);
+        }
+    }
+
+    private void qualityBackstagePasses(Item item){
+        increaseQuality(item);
+        if(item.sellIn <= 10) {
+            increaseQuality(item);
+        }
+        if(item.sellIn <= 5){ 
+            increaseQuality(item);
+        }
+        if(item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void qualityConjured (Item item){
+        decreaseQuality(item);
+        decreaseQuality(item);
+        if(item.sellIn < 0){
+            decreaseQuality(item);
+            decreaseQuality(item);
+        }
+    }
+
+    private void qualityNormalItem(Item item){
+        decreaseQuality(item);
+        if(item.sellIn < 0) {
+            decreaseQuality(item);
         }
     }
 }
