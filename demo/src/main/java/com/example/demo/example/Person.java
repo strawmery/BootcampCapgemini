2da81412c98d11b066d5bea5d7503abcab29002e
maria <mgarciasierragrsr@gmail.com>
2025-03-11 09:14:26 +0100

    Added a new class to try testing

---
+++ demo/src/main/java/com/example/demo/example/Person.java
@@ -0,0 +1,13 @@
+package com.example.demo.example;
+
+public class Person {
+
+    public int id;
+    public String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
---
+++ demo/src/test/java/com/example/demo/PersonTest.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.example.Person;
+
+public class PersonTest {
+
+    @Test
+    void createPerson(){
+        var person = new Person(1, "Juan");
+
+        assertNotNull(person);
+        assertAll("Constructor",
+            () -> assertEquals(1, person.id, "id"),
+            () -> assertEquals("Juan", person.name, "name"));
+        assertEquals(1, person.id);
+        assertEquals("Juan", person.name);
+    }
+
+    @Test
+    void wrongPerson(){
+        var person = new Person(1, "juan");
+
+        assertNotNull(person);
+        assertNotEquals(2, person.id);
+        assertNotEquals("pedro",person.name);
+    }
+
+}
