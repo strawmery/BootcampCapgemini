0879f8c887816a936be354bdc7f8b36faeb7e6ca
maria <mgarciasierragrsr@gmail.com>
2025-03-13 11:01:17 +0100

    Implement CommandLineRunner in ProyectoApplication to display example data on startup

--- proyecto/src/main/java/cap/maria/proyecto/ProyectoApplication.java
+++ proyecto/src/main/java/cap/maria/proyecto/ProyectoApplication.java
@@ -1,13 +1,30 @@
 package cap.maria.proyecto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import cap.maria.proyecto.Repositories.ActorRepository;
+
 @SpringBootApplication
-public class ProyectoApplication {
+public class ProyectoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("CommandLineRunner running");
+		exampleData();
+	}
+
+	@Autowired
+	private ActorRepository dao;
+
+	private void exampleData(){
+		dao.findAll().forEach(System.out::println);
 	}
 
 }
