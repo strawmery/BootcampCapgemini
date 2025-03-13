5d84697e9d651731472a3cbfcaadc94a27522bed
maria <mgarciasierragrsr@gmail.com>
2025-03-13 11:18:02 +0100

    insert actor

--- proyecto/src/main/java/cap/maria/proyecto/ProyectoApplication.java
+++ proyecto/src/main/java/cap/maria/proyecto/ProyectoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import cap.maria.proyecto.Entities.Actor;
 import cap.maria.proyecto.Repositories.ActorRepository;
 
 @SpringBootApplication
@@ -24,6 +25,8 @@ public class ProyectoApplication implements CommandLineRunner{
 	private ActorRepository dao;
 
 	private void exampleData(){
+		var actor = new Actor(0,"zendaya", "ejemplo");
+		dao.save(actor);
 		dao.findAll().forEach(System.out::println);
 	}
 
