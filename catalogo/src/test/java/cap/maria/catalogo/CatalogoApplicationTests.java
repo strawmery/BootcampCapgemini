b475657039dbf3752f1162aa5e440b3fb84ac682
maria <mgarciasierragrsr@gmail.com>
2025-03-13 16:24:44 +0100

    initialize catalogo project with basic structure and configuration files

---
+++ catalogo/src/test/java/cap/maria/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.maria.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
