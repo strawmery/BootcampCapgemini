7b9928902eace2c9e13327eebb2b33d969511d6d
maria <mgarciasierragrsr@gmail.com>
2025-03-13 16:53:34 +0100

    add DomainService interface for CRUD operations with exception handling

---
+++ catalogo/src/main/java/cap/maria/catalogo/Services/DomainService.java
@@ -0,0 +1,21 @@
+package cap.maria.catalogo.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.dao.DuplicateKeyException;
+
+import cap.maria.catalogo.Exceptions.InvalidDataException;
+
+public interface DomainService <E, K>{
+    List<E> getall();
+    Optional <E> getOne(K id);
+    
+    E add(E item) throws DuplicateKeyException, InvalidDataException;
+
+    E update(E item) throws DuplicateKeyException, InvalidDataException;
+
+    void delete(E item) throws InvalidDataException;
+    void deleteById(K id) throws InvalidDataException;
+
+}
