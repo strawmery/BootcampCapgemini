f1e3e3bd318bd9a7484c98bc5b5956bdc0d0d722
maria <mgarciasierragrsr@gmail.com>
2025-03-13 16:17:26 +0100

    deleted the hole project to do one new

--- proyecto/src/main/java/cap/maria/proyecto/contracts/repositories/ActorRepository.java
+++
@@ -1,9 +0,0 @@
-package cap.maria.proyecto.contracts.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cap.maria.proyecto.core.Entities.Actor;
-
-public interface ActorRepository extends JpaRepository <Actor, Integer>{
-
-}
--- proyecto/src/main/java/cap/maria/proyecto/contracts/services/ActorService.java
+++
@@ -1,8 +0,0 @@
-package cap.maria.proyecto.contracts.services;
-
-import cap.maria.proyecto.core.Entities.Actor;
-import cap.maria.proyecto.core.contracts.services.DomainService;
-
-public interface ActorService extends DomainService<Actor,Integer>{
-
-}
