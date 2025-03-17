0aee77d03f915a5e4c7874e261a8181be674934b
maria <mgarciasierragrsr@gmail.com>
2025-03-17 09:04:57 +0100

    ``` No code changes made. ```

--- catalogo/src/main/java/cap/maria/catalogo/Entities/Actor.java
+++ catalogo/src/main/java/cap/maria/catalogo/Entities/Actor.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.sql.Timestamp;
 import java.util.List;
