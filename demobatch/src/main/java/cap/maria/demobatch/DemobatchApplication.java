a8def1c6634f66e1b31ba348cf01649cd4c5f8d2
maria <mgarciasierragrsr@gmail.com>
2025-03-18 12:08:08 +0100

    Initialize demobatch project with Spring Boot; add application properties, Maven wrapper, and basic test setup

---
+++ demobatch/src/main/java/cap/maria/demobatch/DemobatchApplication.java
@@ -0,0 +1,13 @@
+package cap.maria.demobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobatchApplication.class, args);
+	}
+
+}
---
+++ demobatch/src/test/java/cap/maria/demobatch/DemobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.maria.demobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
