f902b0a965af378cb42938ad785368ae1e9b6648
maria <mgarciasierragrsr@gmail.com>
2025-03-24 15:16:43 +0100

    new project

---
+++ labo/src/main/java/cap/maria/labo/LaboApplication.java
@@ -0,0 +1,13 @@
+package cap.maria.labo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboApplication.class, args);
+	}
+
+}
---
+++ labo/src/test/java/cap/maria/labo/LaboApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.maria.labo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
