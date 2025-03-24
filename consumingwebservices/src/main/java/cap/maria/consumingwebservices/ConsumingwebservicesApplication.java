89d74a452bc7eedf36dd950c400b8fd33b954a83
maria <mgarciasierragrsr@gmail.com>
2025-03-24 16:51:22 +0100

    Initialize Spring Boot project with basic configuration and structure

---
+++ consumingwebservices/src/main/java/cap/maria/consumingwebservices/ConsumingwebservicesApplication.java
@@ -0,0 +1,13 @@
+package cap.maria.consumingwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingwebservicesApplication.class, args);
+	}
+
+}
---
+++ consumingwebservices/src/test/java/cap/maria/consumingwebservices/ConsumingwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.maria.consumingwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
