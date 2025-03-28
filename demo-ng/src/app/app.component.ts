7d8fbf93e934326ac8aba4d406b1f226844e1850
maria <mgarciasierragrsr@gmail.com>
2025-03-28 13:44:06 +0100

    Integrate NotificationComponent into AppComponent and update module imports

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
+import { NotificationComponent } from './main/notification/notification.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NotificationComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,4 +19,5 @@ export class AppComponent {
     out.info('Es un info')
     out.log('Es un log')
   }
+  
 }
