bd2ff8d328f3c389c80190f5280dea37c92ead8d
maria <mgarciasierragrsr@gmail.com>
2025-03-28 13:45:12 +0100

    Add DemosComponent with template, styles, and unit tests

---
+++ demo-ng/src/app/demos/demos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demos',
+  imports: [],
+  templateUrl: './demos.component.html',
+  styleUrl: './demos.component.css'
+})
+export class DemosComponent {
+
+}
