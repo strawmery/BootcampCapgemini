eb951c20f58abc34c69a6f83841ff95915af9398
maria <mgarciasierragrsr@gmail.com>
2025-03-28 11:41:10 +0100

    Integrate LoggerService into AppComponent for logging error, warning, info, and log messages

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoggerService } from '@my/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'world';
+
+  constructor(out: LoggerService) {
+    out.error('Es un error')
+    out.warn('Es un warn')
+    out.info('Es un info')
+    out.log('Es un log')
+  }
 }
