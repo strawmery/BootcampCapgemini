3986afe10cb5bfbc05a96efd531bfd58e6b165a6
maria <mgarciasierragrsr@gmail.com>
2025-03-31 11:41:04 +0200

    Add HomeComponent with template, styles, and unit tests

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -15,11 +15,11 @@ import { NotificationModalComponent } from './main';
 export class AppComponent {
   title = 'world';
 
-  constructor(out: LoggerService) {
-    out.error('Es un error')
-    out.warn('Es un warn')
-    out.info('Es un info')
-    out.log('Es un log')
-  }
+  // constructor(out: LoggerService) {
+  //   out.error('Es un error')
+  //   out.warn('Es un warn')
+  //   out.info('Es un info')
+  //   out.log('Es un log')
+  // }
   
 }
