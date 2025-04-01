fde9b2d817482fa75961b3ecc630aba813b1ae9a
maria <mgarciasierragrsr@gmail.com>
2025-04-01 09:26:13 +0200

    Enhance DemosComponent with new input fields, buttons, and dynamic content rendering

--- demo-ng/src/app/demos/demos.component.ts
+++ demo-ng/src/app/demos/demos.component.ts
@@ -1,10 +1,12 @@
 import { Component, computed, OnDestroy, OnInit, signal, ɵINPUT_SIGNAL_BRAND_WRITE_TYPE } from '@angular/core';
 import { NotificationService, NotificationType } from '../common-services';
 import { Unsubscribable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-demos',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './demos.component.html',
   styleUrl: './demos.component.css'
 })
@@ -49,6 +51,14 @@ export class DemosComponent implements OnInit, OnDestroy{
   cambia( ) {
     this.resultado.update(valor => valor )
   }
+
+  add(provincia: string) {
+    const id = this.listado()[this.listado().length - 1].id + 1;
+    this.listado.update(valor => [...valor, { id, nombre: provincia }]);
+    this.idNombre.set(id);
+  }
+
+  calcula(a: number, b:number) { return a + b; }
 
   ngOnInit(): void {
     this.suscriptor = this.vm.Notificacion.subscribe(n => {
