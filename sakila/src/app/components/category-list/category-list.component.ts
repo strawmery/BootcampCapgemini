06103c43a71e2da4ee183c980c54f621aa757266
maria <mgarciasierragrsr@gmail.com>
2025-04-02 15:53:38 +0200

    Add CategoryList component with HTML, CSS, and unit tests

---
+++ sakila/src/app/components/category-list/category-list.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+
+@Component({
+  selector: 'app-category-list',
+  imports: [],
+  templateUrl: './category-list.component.html',
+  styleUrl: './category-list.component.css'
+})
+export class CategoryListComponent implements OnInit{
+
+  category: any[] = [];
+  newCategory: string = '';
+  page = 0;
+  size = 5;
+  totalPages = 0;
+  editCategory: any ={ id:null, name:''};
+
+  constructor (private categoryService: CategoryService) {}
+
+  ngOnInit() {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias() {
+    this.categoryService.obtenerCategoriasPaginadas(this.page, this.size).subscribe(data => {
+      this.category = data.content;
+      this.totalPages = data.totalPages;
+    });
+  }
+
+  paginaAnterior() {
+    if (this.page > 0) {
+      this.page--;
+      this.obtenerCategorias();
+    }
+  }
+
+  paginaSiguiente() {
+    if (this.page < this.totalPages - 1) {
+      this.page++;
+      this.obtenerCategorias();
+    }
+  }
+
+  crearCategorias(){
+    if(this.newCategory.trim()){
+      this.categoryService.crearCategorias({name: this.newCategory}).subscribe(() => {
+        this.obtenerCategorias();
+        this.newCategory = '';
+      })
+    }
+  }
+
+  prepararEdicion(categoria: any) {
+    this.editCategory = { id: categoria.categoryId, name: categoria.name };
+  }
+
+  actualizarCategoria() {
+    if (this.editCategory.name.trim()) {
+      this.categoryService.actualizarCategoria(this.editCategory.id, { name: this.editCategory.name })
+        .subscribe(() => {
+          this.obtenerCategorias();
+          this.editCategory = { id: null, name: '' };
+        });
+    }
+  }
+  
+
+  eliminarCategoria(id: number) {
+    this.categoryService.eliminarCategoria(id).subscribe(() => {
+      this.obtenerCategorias(); // Recargar lista
+    });
+  }
+
+}
--- sakila/src/app/services/category.service.ts
+++ sakila/src/app/services/category.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+ 
+  private apiUrl = 'http://localhost:8080/categorias/v1';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obtenerCategorias(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl)
+  }
+
+  obtenerCategoriasPaginadas(page: number = 0, size: number = 10, sortBy: string = 'categoryId'): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?page=${page}&size=${size}&sortBy=${sortBy}`);
+  }
+
+  obtenerCategoriaPorId(id: number): Observable<any[]> {
+    return this.http.get<any>('${this.apiUrl}/${id}');
+  }
+
+  crearCategorias(categoria: {name:string}): Observable<any> {
+    return this.http.post<any>(this.apiUrl, categoria);
+  }
+
+  actualizarCategoria(id:number, categoria: {name: string}): Observable<any> {
+    return this.http.put<any>('${this.apiUrl}/${id}', categoria);
+  }
+
+  eliminarCategoria(id:number): Observable<any> {
+    return this.http.delete<any>('${this.apiUrl}/${id');
+  }
 }
