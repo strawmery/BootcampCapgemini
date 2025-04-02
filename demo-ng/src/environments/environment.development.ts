61095aa7ec18a161beda7d983ac87f49d3452b26
maria <mgarciasierragrsr@gmail.com>
2025-04-02 08:23:22 +0200

    Add ContactosModule and ServiciosService with unit tests

--- demo-ng/src/environments/environment.development.ts
+++ demo-ng/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     ERROR_LEVEL: 99,
-    apiUrl: 'http://localhost:8010/',
+    apiURL: 'http://localhost:4321/api/',
     securityApiURL: 'http://localhost:8091/',
 };
